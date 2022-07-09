b366a151fc17a67268852f3d050f4cfcc81a4a93
Dilshod Sadiev <dilshod.sadiev@gmail.com>
2022-07-09 15:44:45 -0400

    Selection Sort

--- Sorting/Program.cs
+++ Sorting/Program.cs
@@ -6,8 +6,9 @@ namespace Sorting
     {
         static void Main(string[] args)
         {
-            var arr = new int[]{8,5, 6, 1, 9, 2};
-            new BubbleSort().sort(arr);
+            var arr = new int[]{8, 5, 3, 6, 1, 3, 9, 2};
+            //new BubbleSort().sort(arr);
+            new SelectionSort().sort(arr);
             Console.WriteLine(String.Join(',', arr));
         }
     }
---
+++ Sorting/SelectionSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    internal class SelectionSort
+    {
+        //              Best        Worst
+        //Passes        O(n)        O(n)
+        //Comparisons   O(n)        O(n)
+        //Total         O(n^2)      O(n^2)
+        //              Quadratic   Quadratic
+        public void sort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                var minIndex=Array.IndexOf(arr, arr.Skip(i).ToArray().Min());
+                swap(minIndex, i, arr);
+            }
+        }
+
+        private void swap(int index1, int index2, int[] arr)
+        { 
+            var temp=arr[index1];
+            arr[index1] = arr[index2];
+            arr[index2] = temp;
+        }
+    }
+}

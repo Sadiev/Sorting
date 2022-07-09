b3c4d73b795ec17eae230024315c0ae3b7d2cbe0
Shod <dilshod.sadiev@outlook.com>
2022-07-09 15:00:01 -0400

    Add project files.

---
+++ Sorting/BubbleSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sorting
+{
+    class BubbleSort
+    {
+        public void sort(int[] arr)
+        {
+            bool isSorted;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                isSorted = true;
+                for (int j = 1; j < arr.Length - i; j++)
+                {
+                    if (arr[j-1] > arr[j])
+                    {
+                        swap(j, arr);
+                        isSorted = false;
+                    }
+                }
+                if(isSorted)
+                    break;
+            }
+        }
+        private void swap(int index, int[] arr)
+        {
+            var temp = arr[index];
+            arr[index] = arr[index - 1];
+            arr[index - 1] = temp;
+        }
+    }
+}
---
+++ Sorting/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[]{8,5, 6, 1, 9, 2};
+            new BubbleSort().sort(arr);
+            Console.WriteLine(String.Join(',', arr));
+        }
+    }
+}

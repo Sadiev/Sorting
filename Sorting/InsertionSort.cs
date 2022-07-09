7836e37c76222840281f1760cfa51175e0f251b9
Dilshod Sadiev <dilshod.sadiev@gmail.com>
2022-07-09 16:13:49 -0400

    Insertion sort

---
+++ Sorting/InsertionSort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    //                  Best        Worst
+    //Iteration         O(n)        O(n)
+    //Shift items       O(1)        O(n)
+    //Total             O(n)        O(n^2)
+    //                  Linear      Quadratic
+    internal class InsertionSort
+    {
+        public void sort(int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                var current=arr[i];
+                var j=i-1;
+                while (j>=0 && arr[j]>current)
+                {
+                    arr[j+1]=arr[j];
+                    j--;
+                }
+                arr[j+1]=current;
+            }
+        }
+    }
+}
--- Sorting/Program.cs
+++ Sorting/Program.cs
@@ -8,7 +8,8 @@ namespace Sorting
         {
             var arr = new int[]{8, 5, 3, 6, 1, 3, 9, 2};
             //new BubbleSort().sort(arr);
-            new SelectionSort().sort(arr);
+            //new SelectionSort().sort(arr);
+            new InsertionSort().sort(arr);
             Console.WriteLine(String.Join(',', arr));
         }
     }

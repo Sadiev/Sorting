0b29d5815f583e51db54dd2c2a9da2a5ec43c432
Dilshod Sadiev <dilshod.sadiev@gmail.com>
2022-07-09 16:46:59 -0400

    Merge sort

---
+++ Sorting/MergeSort.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    //                  Best            Worst
+    //Dividing          O(log n)        O(log n)
+    //Merging           O(n)            O(n)
+    //Total             O(n log n)      O(n log n)
+    //Space             O(n)            O(n)
+    internal class MergeSort
+    {
+        public void sort(int[] arr)
+        {
+            if(arr.Length<2)
+                return;
+            //Divide the array into half
+            var middle = arr.Length / 2;
+
+            var left = new int[middle];
+            for (var i = 0; i < middle; i++)
+                left[i] = arr[i];
+
+            var right = new int[arr.Length-middle];
+            for (int i = middle; i < arr.Length; i++)
+                right[i-middle] = arr[i];
+
+            //Sort each half
+            sort(left);
+            sort(right);
+
+            //Merge the result
+            merge(left, right, arr);
+        }
+
+        private void merge(int[] left, int[] right, int[] result)
+        {
+            var i = 0;
+            var j = 0;
+            var k = 0;
+            while (i < left.Length && j < right.Length)
+            {
+                if (left[i] <= right[j])
+                {
+                    result[k++] = left[i++];
+                }
+                else
+                {
+                    result[k++] = right[j++];
+                }
+            }
+            while (i<left.Length)
+            {
+                result[k++] = left[i++];
+            }
+            while (j < right.Length)
+            {
+                result[k++] = right[j++];
+            }
+        }
+    }
+}
--- Sorting/Program.cs
+++ Sorting/Program.cs
@@ -9,7 +9,8 @@ namespace Sorting
             var arr = new int[]{8, 5, 3, 6, 1, 3, 9, 2};
             //new BubbleSort().sort(arr);
             //new SelectionSort().sort(arr);
-            new InsertionSort().sort(arr);
+            //new InsertionSort().sort(arr);
+            new MergeSort().sort(arr);
             Console.WriteLine(String.Join(',', arr));
         }
     }
